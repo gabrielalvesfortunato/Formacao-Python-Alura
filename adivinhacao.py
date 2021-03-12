f3f87fa97df52d64c7cf0d62a49d7a3ffe99fc4e
Gabriel Alves <gabrielalvesfortunato@gmail.com>
2021-03-12 08:24:48 -0300

    Implementando sistema de níveis e pontuação

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,17 +7,35 @@ print("Bem vindo ao jogo de Adivinhação", end="!\n")
 print("*********************************")
 
 # Declarando as variaveis
-tentativas_restantes = 5
-numero_secreto = randint(0, 20)
+tentativas_restantes = 0
+numero_secreto = randint(1, 50)
 chute_correto = False
+pontos = 100
+
+print("\nQual o nivel de dificuldade?")
+print("(1) Facil   (2) Medio   (3) Difícil")
+
+nivel = int(input("\nDefina o nível: "))
+
+if nivel == 1:
+    tentativas_restantes = 10
+elif nivel == 2:
+    tentativas_restantes = 7
+elif nivel == 3:
+    tentativas_restantes = 5
+
+print("Número de Tentativas: {}".format(tentativas_restantes))
 
 # Estrutura principal do programa
 for rodada in range(1, tentativas_restantes + 1):
-    chute = int(input("\nDigite um número entre 1 e 20: "))
+    chute = int(input("\nDigite um número entre 1 e 50: "))
     print("Você digitou: {}".format(chute))
 
-    if(chute < 1 or chute > 20):
-        print("Você deve digitar um numero entre 1 e 20!")
+    # Definindo um intervalo valido de valores
+    if(chute < 1 or chute > 50):
+        print("Você deve digitar um numero entre 1 e 50!")
+        tentativas_restantes = tentativas_restantes - 1
+        print("Tentativas Restantes: {}".format(tentativas_restantes))
         continue
 
     # Definindo os criterios de validação
@@ -26,18 +44,23 @@ for rodada in range(1, tentativas_restantes + 1):
     maior = (chute > numero_secreto)
 
     if acertou:
-        print("Parabéns você acertou!")
+        print("\nParabéns você acertou e fez um total de {} pontos!".format(pontos))
         print("\nFim do Jogo")
         break
 
     elif menor:
         print("\nO seu valor foi menor que o numero correto, VOCE ERROU!", end="\n")
         tentativas_restantes = tentativas_restantes - 1
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
         print("Tentativas Restantes: {}".format(tentativas_restantes))
+
 
     elif maior:
         print("\nO seu valor foi maior que o numero correto, VOCE ERROU!", end="\n")
         tentativas_restantes = tentativas_restantes - 1
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
         print("Tentativas Restantes: {}".format(tentativas_restantes))
 
     if tentativas_restantes == 0:
